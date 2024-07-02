a2f2cdc96fc93a7359470598f09884be45380cdf
Bandana Satpathy <bandna.quicklyplatforms@gmail.com>
2024-07-02 11:50:17 +0530

    testing

---
+++ header.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+   
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!--====== Title ======-->
+    <title>Doubly Enchanted</title>
+    
+    <!--====== Favicon Icon ======-->
+    <!-- <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png"> -->
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">  
+    
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/custom-animation.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+  
+  
+</head>
+
+<body>   
+
+    <!--====== OFFCANVAS MENU PART START ======-->
+
+    <div class="off_canvars_overlay">
+                
+    </div>
+    <div class="offcanvas_menu">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="offcanvas_menu_wrapper">
+                        <div class="canvas_close">
+                            <a href="javascript:void(0)"><i class="fa fa-times"></i></a>  
+                        </div>
+                        <div class="offcanvas-brand text-center mb-40">
+                            <img src="assets/images/logo.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== OFFCANVAS MENU PART ENDS ======-->
+   
+    <!--====== APPIE HEADER PART START ======-->
+    
+    <header class="appie-header-area appie-header-2-area appie-sticky">
+        <div class="container">
+            <div class="header-nav-box">
+                <div class="row align-items-center">
+                    <div class="col-lg-2 col-md-4 col-sm-5 col-6 order-1 order-sm-1">
+                        <div class="appie-logo-box">
+                            <a href="#">
+                                <img src="assets/images/logo-2.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-1 col-sm-1 order-3 order-sm-2">
+                        <div class="appie-header-main-menu">
+                        </div>
+                    </div>
+                    <div class="col-lg-4  col-md-7 col-sm-6 col-6 order-2 order-sm-3">
+                        <div class="appie-btn-box text-right">
+                            <a class="main-btn ml-30" href="#contact">Get Started</a>
+                            <div class="toggle-btn ml-30 canvas_open d-lg-none d-block">
+                                <i class="fa fa-bars"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+    <!--====== APPIE HEADER PART ENDS ======-->
---
+++ index.php
@@ -0,0 +1,282 @@
+<?php include 'header.php';?>
+    <!--====== APPIE HERO PART START ======-->
+    
+    <section class="appie-hero-area-2 mb-90">
+        <div class="container pb-5 pt-5">
+            <div class="row align-items-center">
+                <div class="col-lg-5">
+                    <div class="appie-hero-content-2">
+                        <h1 class="appie-title">Ready to launch your app in one, <span>Application</span></h1>
+                        <p>Welcome to Your Personalized Skin Care Journey</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="appie-hero-thumb-3 wow animated fadeInRight" data-wow-duration="2000ms" data-wow-delay="400ms">
+            <img src="assets/images/hero-thumb-3.png" alt="">
+        </div>
+        <div class="hero-shape-1">
+            <img src="assets/images/shape/shape-9.png" alt="">
+        </div>
+        <div class="hero-shape-2">
+            <img src="assets/images/shape/shape-10.png" alt="">
+        </div>
+        <div class="hero-shape-3">
+            <img src="assets/images/shape/shape-11.png" alt="">
+        </div>
+        <div class="hero-shape-4">
+            <img src="assets/images/shape/shape-12.png" alt="">
+        </div>
+    </section>
+    
+    <!--====== APPIE HERO PART ENDS ======-->
+
+    <!--====== APPIE SERRVICE 2 PART START ======-->    
+    <section class="appie-about-area mb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="appie-about-box wow animated fadeInUp" data-wow-duration="2000ms" data-wow-delay="200ms">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="about-thumb">
+                                    <img src="assets/images/about-thumb.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="appie-about-content">
+                                    <h3 class="title">Welcome to Your Personalized Skin Care Journey</h3>
+                                    <p>Unlock the secrets to radiant, healthy skin with our innovative app, designed using the expertise of top dermatologists and the power of advanced AI. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--====== APPIE ABOUT PART ENDS ======-->
+
+    <!--====== APPIE FEATURES 2 PART START ======-->
+    
+    <section class="appie-features-area-2 pt-90 pb-100" id="features">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="appie-section-title appie-section-title-2 text-center">
+                        <h3 class="appie-title">Wherever you need <br> us the most</h3>
+                        <p>The full monty spiffing good time no biggie cack grub fantastic. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-30 align-items-center">
+                <div class="col-lg-6">
+                    <div class="appie-features-boxes">
+                        <div class="appie-features-box-item">
+                            <h4 class="title">Well Integrated</h4>
+                            <p>The bee's knees chancer car boot absolutely.</p>
+                        </div>
+                        <div class="appie-features-box-item item-2">
+                            <h4 class="title">Clean and modern Design</h4>
+                            <p>The bee's knees chancer car boot absolutely.</p>
+                        </div>
+                        <div class="appie-features-box-item item-3">
+                            <h4 class="title">Light and dark mode</h4>
+                            <p>The bee's knees chancer car boot absolutely.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="appie-features-thumb wow animated fadeInRight" data-wow-duration="2000ms" data-wow-delay="200ms">
+                        <img src="assets/images/features-thumb-2.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="features-shape-1">
+            <img src="assets/images/shape/shape-15.png" alt="">
+        </div>
+        <div class="features-shape-2">
+            <img src="assets/images/shape/shape-14.png" alt="">
+        </div>
+        <div class="features-shape-3">
+            <img src="assets/images/shape/shape-13.png" alt="">
+        </div>
+    </section>
+    
+    <!--====== APPIE FEATURES 2 PART ENDS ======-->
+
+
+    <section class="appie-services-2-area pb-100 mt-5" id="service">
+        <div class="container">
+            <div class="row align-items-end">
+                <div class="col-lg-6 col-md-8">
+                    <div class="appie-section-title">
+                        <h3 class="appie-title">How does it work</h3>
+                        <p>The full monty spiffing good time no biggie cack grub fantastic. </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-4">
+                    <div class="appie-section-title text-right">
+                        <a class="main-btn" href="#">View all Features <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="200ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 200ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-bolt"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 item-2 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="400ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 400ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-home"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 item-3 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="600ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 600ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-link"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 item-4 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="200ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 200ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-bell"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 item-5 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="400ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 400ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-cog"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="appie-single-service-2 item-6 mt-30 wow animated fadeInUp animated" data-wow-duration="2000ms" data-wow-delay="600ms" style="visibility: visible; animation-duration: 2000ms; animation-delay: 600ms; animation-name: fadeInUp;">
+                        <div class="icon">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <h4 class="title">Fast and intuitive</h4>
+                        <p>Oxford posh bevvy give us a bell gutted mate spend a penny quaint cockup plastered.</p>
+                        <a href="#">Read Mor <i class="fal fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!--====== APPIE DOWNLOAD PART START ======-->
+    
+    <section class="appie-download-area pt-150 pb-160">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="appie-download-content">
+                        <span>Download Our App</span>
+                        <h3 class="title">App is available <br> for free on app store</h3>
+                        <p>Jolly good quaint james bond victoria sponge happy days cras arse over blatant.</p>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-apple"></i>
+                                    <span>Download for <span>iOS</span></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="item-2" href="#">
+                                    <i class="fab fa-google-play"></i>
+                                    <span>Download for <span>Android</span></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="download-shape-1">
+            <img src="assets/images/shape/shape-15.png" alt="">
+        </div>
+        <div class="download-shape-2">
+            <img src="assets/images/shape/shape-14.png" alt="">
+        </div>
+        <div class="download-shape-3">
+            <img src="assets/images/shape/shape-13.png" alt="">
+        </div>
+    </section>
+    
+    <!--====== APPIE SPONSER PART ENDS ======-->
+   <section class="contact-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 mb-5">
+                            <h3>Get in touch</h3>
+                            <p>
+                                Looking for help? Fill the form and start a new adventure.
+                            </p>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="contact--info-area appie-single-counter mt-30 wow animated fadeInUp ">
+                            <div class="single-info">
+                                <h5>Headquaters</h5>
+                                <p>
+                                    <i class="fal fa-home"></i>
+                                    744 New York Ave, Brooklyn, Kings,<br> New York 10224
+                                </p>
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="contact--info-area appie-single-counter mt-30 wow animated fadeInUp">
+                            <div class="single-info">
+                                <h5>Phone</h5>
+                                <p>
+                                    <i class="fal fa-phone"></i>
+                                    (+642) 245 356 432<br>
+                                    (+420) 336 476 328
+                                </p>
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="contact--info-area appie-single-counter mt-30 wow animated fadeInUp">
+                            <div class="single-info">
+                                <h5>Support</h5>
+                                <p>
+                                    <i class="fal fa-envelope"></i>
+                                    bisy@support.com<br>
+                                    help@education.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+<?php include 'footer.php';?>
