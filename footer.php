0a04b321f3c505a105c29a9e8f5764090cc1fdb3
Bandana Satpathy <bandna.quicklyplatforms@gmail.com>
2024-07-02 10:57:15 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+    <!--====== APPIE FOOTER PART START ======-->
+    
+    <section class="appie-footer-area p-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                        <div class="copyright-text text-center">
+                            <p>Copyright © <?php echo date("Y"); ?> Doubly. All rights reserved.</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--====== APPIE FOOTER PART ENDS ======-->    
+    
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
